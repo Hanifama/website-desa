13f8d821b2493277c9b7022b4798700e5a3ad833
hanifama <hanifamafisial.com>
2024-09-29 00:20:23 +0700

    adjust

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -22,16 +22,16 @@ export default function Header({data}) {
     <div className="container min-w-full h-auto py-20 pt-28 bg-gradient-to-r from-[#e0f7fa] to-[#ffffff]">
       <div className="outer-container flex flex-col md:flex-row justify-center items-center md:items-start w-full">
         <div className="container__right flex flex-col md:flex-row p-6 md:p-8 w-full">          
-          <div className="content flex-1 text-left space-y-2 py-10 px-10">
-            <h4 className="text-lg font-semibold text-[#b95f30] uppercase tracking-wide">
+          <div className="content flex-1 text-left space-y-2 py-8 px-10">
+            <h4 className="text-sm font-semibold text-[#b95f30] uppercase tracking-wide">
               {profileData.province} - {profileData.district}
             </h4>
-            <h2 className="text-3xl md:text-5xl font-extrabold text-[#4a3f3f] leading-tight relative">
+            <h2 className="text-2xl md:text-5xl font-extrabold text-[#4a3f3f] leading-tight relative">
               {profileData.name}
               <span className="absolute top-10 md:top-12 left-0 w-12 h-1 bg-[#b95f30]"></span>
             </h2>
             <p className="leading-relaxed text-[#5a5a5a] max-w-md">
-              Desa {profileData.village}
+              {/* Desa {profileData.village} */}
             </p>
           </div>
 
